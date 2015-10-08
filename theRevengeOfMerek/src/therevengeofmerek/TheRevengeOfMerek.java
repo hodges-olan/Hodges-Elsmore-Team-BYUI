a128d2e0e821f3a4f0bedfb3cfb624640ae2093b
maryelsmore <maryelsmore@10.0.1.12>
2015-10-07 23:55:05 -0700

    10/7/2015 - Initial commit to repository

---
+++ theRevengeOfMerek/src/therevengeofmerek/TheRevengeOfMerek.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package therevengeofmerek;
+
+import byui.cit260.theRevengeOfMerek.model.Character;
+
+/**
+ *
+ * @author maryelsmore
+ */
+public class TheRevengeOfMerek {
+
+    public static void main(String[] args) {
+       Character characterOne = new Character();
+       
+       characterOne.setName("Merek");
+       
+       String characterInfo = characterOne.toString();
+       System.out.println(characterInfo);
+    }
+    
+}
